21806d00b0ce940c88a7206fc6062a8aef2049f8
Lautaro Colazo <lautarocolazo1@gmail.com>
2023-09-16 20:51:34 -0600

    w01 prove

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade percentage?: ");
+        string gradePercentage = Console.ReadLine();
+        string letter;
+
+        if (int.Parse(gradePercentage) >= 90) 
+        {
+            letter = "A";
+        }
+        else if (int.Parse(gradePercentage) >= 80)
+        {
+            letter = "B";
+        }
+        else if (int.Parse(gradePercentage) >= 70)
+        {
+            letter = "C";
+        }
+        else if (int.Parse(gradePercentage) >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+        
+        Console.WriteLine($"Your grade is equivalent to: {letter}");
+
+        if (int.Parse(gradePercentage) >= 70)
+        {
+            Console.WriteLine("Congratulations for passing the course");
+        }
+        else
+        {
+            Console.WriteLine("Keep working hard!");
+        }
     }
 }
