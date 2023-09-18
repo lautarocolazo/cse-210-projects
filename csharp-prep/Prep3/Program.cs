61ed8e9408b45a3e8339e5b69d386511d837ccf3
Lautaro Colazo <lautarocolazo1@gmail.com>
2023-09-18 10:45:19 -0600

    adding c# prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 11);
+
+        int guessNumber = -1;
+
+        while (magicNumber != guessNumber)
+        {
+
+            Console.WriteLine("What is your guess number?: ");
+            guessNumber = int.Parse(Console.ReadLine());
+
+            if (guessNumber > magicNumber)
+            {
+                Console.WriteLine("Guess lower");
+            }
+            else if (guessNumber < magicNumber)
+            {
+                Console.WriteLine("Guess higher");
+            }
+            else
+            {
+                Console.WriteLine("You guessed right!");
+            }
+        }
+        Console.WriteLine("End of the game.");
     }
 }
