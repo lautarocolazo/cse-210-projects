2c064298af01da8d8972f438e2037d8d2160d0c8
Lautaro Colazo <lautarocolazo1@gmail.com>
2023-09-18 11:50:26 -0600

    adding prep 5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,48 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        
+        string name = PromptUserName();
+
+        int favoriteNumber = PromptUserNumber();
+
+        int squareNumber = SquareNumber(favoriteNumber);
+
+        DisplayResult(name, squareNumber);
+        
+    }
+    static void DisplayWelcome()
+    {
+       Console.WriteLine("Welcome to the program!"); 
+    }
+
+    static string PromptUserName()
+    {
+        Console.WriteLine("Please enter your name: ");
+        string name = Console.ReadLine();
+
+        return name;
+        
+
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.WriteLine("Please enter your favorite number: ");
+        int favoriteNumber = int.Parse(Console.ReadLine());
+
+        return favoriteNumber;
+
+    }
+
+    static int SquareNumber(int number)
+    {
+        return number * number;
+    }
+
+    static void DisplayResult(string name, int number)
+    {
+        Console.WriteLine($"{name}, the square of your number is {number}");
     }
 }
