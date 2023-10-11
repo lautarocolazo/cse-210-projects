a89af1f37910f29518758dec65268236f4595f22
Lautaro Colazo <lautarocolazo1@gmail.com>
2023-10-10 19:15:50 -0600

    learning activity 3 - encapsulation

---
+++ inclass/fraction/Program.cs
@@ -0,0 +1,16 @@
+﻿Fraction fraction_no_parameters =  new Fraction();
+Fraction fraction_one_parameter =  new Fraction(5);
+Fraction fraction_two_parameter =  new Fraction(3, 4);
+
+
+Console.WriteLine(fraction_no_parameters.GetTop());
+Console.WriteLine(fraction_no_parameters.GetBottom() + Environment.NewLine);
+
+Console.WriteLine(fraction_one_parameter.GetTop());
+Console.WriteLine(fraction_one_parameter.GetBottom() + Environment.NewLine);
+
+Console.WriteLine(fraction_two_parameter.GetTop());
+Console.WriteLine(fraction_two_parameter.GetBottom() + Environment.NewLine);
+
+Console.WriteLine(fraction_two_parameter.GetFractionString());
+Console.WriteLine(fraction_two_parameter.GetDecimalValue());
