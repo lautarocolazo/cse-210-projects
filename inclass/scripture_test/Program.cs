10f98222060e2b1a200b06da72e8dfcfb528cfce
Lautaro Colazo <lautarocolazo1@gmail.com>
2023-10-14 18:23:38 -0600

    classes Word, Reference, and Scripture

---
+++ inclass/scripture_test/Program.cs
@@ -0,0 +1,18 @@
+﻿Scripture scripture = new Scripture(new Reference("Genesis", 1, 1, 2), "In the beginning God created the heavens and the earth.");
+
+while (!scripture.IsCompletelyHidden())
+{
+    Console.WriteLine(scripture.GetRenderedReference());
+    Console.WriteLine(scripture.GetRenderedText());
+    Console.WriteLine("Please press enter to continue or 'quit' to finish");
+
+    string input = Console.ReadLine();
+
+    if (input.ToLower() == "quit")
+    {
+        break;
+    }
+    scripture.HideWords(3);
+}
+
+Console.WriteLine("End of program");
---
+++ inclass/scripture_test/Scripture.cs
@@ -0,0 +1,72 @@
+public class Scripture
+{
+    private Reference _reference;
+    private List<Word> _words;
+    private Random random;
+
+    public Scripture(Reference reference, string text)
+    {
+        _reference = reference;
+        _words = new List<Word>();
+        random = new Random();
+
+        string[] wordArray = text.Split(' ');
+
+        foreach (string wordText in wordArray)
+        {
+            _words.Add(new Word(wordText));
+        }
+    }
+
+    public void HideWords(int count)
+    {
+        List<Word> visibleWords = new List<Word>();
+
+        foreach (Word word in _words)
+        {
+            if (!word.GetIsHidden())
+            {
+                visibleWords.Add(word);
+            }
+        }
+
+        for (int i = 0; i < count && visibleWords.Count > 0; i++)
+        {
+            int index = random.Next(visibleWords.Count);
+            Word wordToHide = visibleWords[index];
+            wordToHide.Hide();
+            visibleWords.RemoveAt(index);
+        }
+    }
+
+    public string GetRenderedText()
+    {
+        List<string> renderedWords = new List<string>();
+
+        foreach (Word word in _words)
+        {
+            renderedWords.Add(word.GetRenderedText());
+        }
+
+        return string.Join(" ", renderedWords);
+    }
+
+    public string GetRenderedReference()
+    {
+        return $"{_reference.GetBook()} {_reference.GetChapter()}: {_reference.GetVerse()}-{_reference.GetEndVerse()}";
+    }
+
+    public bool IsCompletelyHidden()
+    {
+        foreach (Word word in _words)
+        {
+            if (!word.GetIsHidden())
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
+
