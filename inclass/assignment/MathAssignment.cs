5a52d503db49d3ffe92a63106bb3704e78a7b209
Lautaro Colazo <lautarocolazo1@gmail.com>
2023-10-23 11:45:58 -0600

    assignment class with inheritance class

---
+++ inclass/assignment/MathAssignment.cs
@@ -0,0 +1,18 @@
+public class MathAssignment : Assignment
+{
+  protected string _textbookSection;
+  protected string _problems;
+
+  public MathAssignment(string studentName, string topic, string textbookSection, string problems) : base(studentName, topic)
+  {
+    _textbookSection = textbookSection;
+    _problems = problems;
+  }
+
+  public string GetHomeworkList()
+  {
+    return $"Section {_textbookSection} Problems {_problems}";
+  }
+
+}
+
---
+++ inclass/assignment/Program.cs
@@ -0,0 +1,10 @@
+﻿MathAssignment math = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+
+Console.WriteLine(math.GetSummary());
+Console.WriteLine(math.GetHomeworkList());
+
+WritingAssignment writing = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II by  Mary Waters");
+
+Console.WriteLine(writing.GetSummary());
+Console.WriteLine(writing.GetWritingInformation());
+
