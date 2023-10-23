a3a4656753a9e2b73971a50178c610c20d06d8a1
Lautaro Colazo <lautarocolazo1@gmail.com>
2023-10-23 11:02:42 -0600

    in-class activity applying inheritance

---
+++ inclass/loanable/Loanable.cs
@@ -0,0 +1,19 @@
+public class Loanable
+{
+  protected bool _isCheckedIn = true;
+
+  public void CheckOut()
+  {
+    _isCheckedIn = false;
+  }
+
+  public void CheckIn()
+  {
+    _isCheckedIn = true;
+  }
+
+  public virtual void Display()
+  {
+    Console.WriteLine($"Available: {_isCheckedIn}");
+  }
+}
---
+++ inclass/loanable/Program.cs
@@ -0,0 +1,17 @@
+﻿Loanable cat = new Loanable();
+
+Console.WriteLine("This is the cat.");
+cat.Display();
+cat.CheckOut();
+cat.Display();
+cat.CheckIn();
+
+Console.WriteLine("This is the DVD");
+Dvd kungFuPanda = new Dvd("Kung Fu Panda");
+
+kungFuPanda.Display();
+kungFuPanda.CheckOut();
+kungFuPanda.Display();
+kungFuPanda.CheckIn();
+
+Console.WriteLine("This is the book");
